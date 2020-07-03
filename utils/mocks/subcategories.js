91480b1ccf08dbf907756ea9d76e3d9b56225fd1
Carlos Enrique Ramirez Flores <linux.carl@gmail.com>
2020-07-03 17:01:42 -0500

    refactor (#11) added mocks categories and subcategories by products

--- utils/mocks/subcategories.js
+++ utils/mocks/subcategories.js
@@ -167,5 +167,114 @@ const subcategories = [
     url_image:
       'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750102311057L.jpg',
   },
+
+  {
+    id: 25,
+    subcategory: 'Refrescos',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00000007500761L.jpg',
+  },
+  {
+    id: 26,
+    subcategory: 'Jugos y Néctares',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750104007015L.jpg',
+  },
+  {
+    id: 27,
+    subcategory: 'Energizantes e Hidratantes',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00003673132100L.jpg',
+  },
+  {
+    id: 28,
+    subcategory: 'Agua',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750108680107L.jpg',
+  },
+  {
+    id: 29,
+    subcategory: 'Licores',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00000007500761L.jpg',
+  },
+  {
+    id: 30,
+    subcategory: 'Vinos',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00800057043540L.jpg',
+  },
+  {
+    id: 31,
+    subcategory: 'Cervezas',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750106411254L.jpg',
+  },
+  {
+    id: 32,
+    subcategory: 'Postres Congelados',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750113093439L.jpg',
+  },
+  {
+    id: 33,
+    subcategory: 'Comida Fácil',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00066417700009L.jpg',
+  },
+  {
+    id: 34,
+    subcategory: 'Hielo',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00040009176656L.jpg',
+  },
+  {
+    id: 35,
+    subcategory: 'Frutas y Verduras Congeladas',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750104790590L.jpg',
+  },
+  {
+    id: 35,
+    subcategory: 'Medicamentos de Patente',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750116501081L.jpg',
+  },
+  {
+    id: 37,
+    subcategory: 'Cuidado Personal',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00065024002056L.jpg',
+  },
+  {
+    id: 38,
+    subcategory: 'Vitaminas y Suplementos',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750116501178L.jpg',
+  },
+  {
+    id: 39,
+    subcategory: 'Café y Té Preparado',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750105531787L.jpg',
+  },
+  {
+    id: 40,
+    subcategory: 'Sushi',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00020616900000L.jpg',
+  },
+  {
+    id: 41,
+    subcategory: 'Aves',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00020104100000L.jpg',
+  },
+  {
+    id: 42,
+    subcategory: 'Mariscos',
+    url_image:
+      'https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750179160078L.jpg',
+  },
 ];
 module.exports = subcategories;
