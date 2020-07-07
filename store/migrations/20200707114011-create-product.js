35a33b5ae9dbfd4dfa64766caeeff5ef1e7be0a2
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-07-07 07:02:53 -0500

    add (#24) add migrations, model and seeders of products

---
+++ store/migrations/20200707114011-create-product.js
@@ -0,0 +1,59 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      deleted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      descripcion: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      urlImage: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      measureId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      subcategoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Products');
+  }
+};
