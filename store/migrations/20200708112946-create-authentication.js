5cb76945b402078b4ac6336f6849d761530f428f
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-07-08 14:07:29 -0500

    add (#29) migrated and model database product

--- store/migrations/20200708112946-create-authentication.js
+++ store/migrations/20200708112946-create-authentication.js
@@ -11,7 +11,7 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'Users',
+          model: 'users',
           key: 'id'
         }
       },
