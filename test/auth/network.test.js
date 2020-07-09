33952f233103d507be672c99f0899b700d92c932
Carlos Enrique Ramirez Flores <linux.carl@gmail.com>
2020-07-09 07:18:54 -0500

    feat (#19) added test by auth

---
+++ test/auth/network.test.js
@@ -0,0 +1,33 @@
+const testServer = require('../../utils/testServer');
+
+describe('routes - users', function () {
+    const route = require('../../api/components/auth/network')
+    const request = testServer(route);
+    describe('POST /auth', function () {
+        it("should respond with status 201 - sign up user", function(done) {
+            const signUp = {
+                "email": "chumble9@reddit.com",
+                "firstName": "Carlos",
+                "lastName": "Ramírez",
+                "rol":"admin",
+                "password":"secret12345"
+            }
+            request.post("/api/sign-up/").send(signUp).end((err, res) => {
+              expect(res.body).toMatchObject({error: false, status: 201, body: expect.any(Array) });
+              done();
+            });
+        });
+        it("should respond with status 200 - sign in user", function(done) {
+            const signIn = {
+                "email": "chumble9@reddit.com",
+                "password":"secret"
+            }
+            request.post("/api/sign-in/").send(signIn).end((err, res) => {
+              expect(res.body).toMatchObject({error: false, status: 200, body: expect.any(Object) });
+              expect(res.body.body).toMatchObject({ token: expect.any(String),user: expect.any(Object) });
+              done();
+            });
+        });
+    });
+});
+
