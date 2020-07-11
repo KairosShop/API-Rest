5979d5e43418a784b3f39f618f33c0822a1aa632
Carlos Enrique Ramirez Flores <linux.carl@gmail.com>
2020-07-11 15:32:44 -0500

    feat (#34) added store by orders and details orders
    feat (#34) added config in postgres by orders and orders details

--- api/components/orders/controller.js
+++ api/components/orders/controller.js
@@ -5,25 +5,29 @@ module.exports = function (injectedStore) {
     store = require('../../../store/mocks');
   }
   async function getOrders(filter={}) {
-    const carts = await store.getAll(TABLE, filter);
-    return carts || [];
+    const orders = await store.getAll(TABLE, filter);
+    return orders || [];
   }
 
   async function getOrder(id) {
-    const cart = await store.getOne(TABLE, id);
-    return cart || [];
+    const order = await store.getOne(TABLE, id);
+    return order || [];
   }
   async function createOrder(orderData) {
     const products = orderData.products;
     delete orderData.products;
 
-    orderData.status = 'in process';
+    orderData.status = 'IN PROCESS';
+    orderData.active = true;
+    orderData.deleted = false;
  
     const { id: orderId } = await store.create(TABLE, orderData);
     for await (const product of products) {
       try {
         let productDetail =  { ... product };
         productDetail.orderId = orderId;
+        productDetail.status = 'STAND BY';
+        productDetail.deleted = false;
         await store.create('ordersDetails', productDetail );
       } catch (error) {
         throw(error.message);
